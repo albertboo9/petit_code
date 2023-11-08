e8ca0277ab2e02114f0c788b0b5369a84ce384a0
albertboo9 <138286951+albertboo9@users.noreply.github.com>
2023-11-08 20:04:21 +0100

    Create TPE_INF233.c
    code de mon groupe de TPE pour un devoir en structure de données

---
+++ TPE_INF233.c
@@ -0,0 +1,9 @@
+/*BAE */
+
+/* Ceci est un programme qui implémente la création et la manipulation d'une pile de données et d'une file de données */
+
+#include <stdio.h>
+
+int main(){
+  printf(" Eight like infinity ");
+}
