715ca23338c63c9a6764b45284e973cea1746a3a
albertboo9 <booalbert60@gmail.com>
2024-10-03 23:08:33 +0100

    Snake

--- TPE_INF233.c
+++
@@ -1,9 +0,0 @@
-/*BAE */
-
-/* Ceci est un programme qui implémente la création et la manipulation d'une pile de données et d'une file de données */
-
-#include <stdio.h>
-
-int main(){
-  printf(" Eight like infinity ");
-}
